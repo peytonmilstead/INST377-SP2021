8cbe430bd63494e174a09f0a79636485b2741eed
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-02-12 12:07:13 -0500

    ADD html for images and button

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,15 @@
 /* Put your javascript in here */
+
+function arrayMethod() {
+    const images = document.querySelectorAll('imageClass');
+    const imageArray = Array.from(images);
+    
+    imageArray.forEach((element) => {
+    console.log(element);
+});
+}
+
+function moveCarousel(){
+    document.addEventListener('click', (event) =>
+    arrayMethod()
+}
