a0de3d9b09cfc53818115d52dc8d38937d6efc2d
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-02-12 16:41:19 -0500

    UPDATE js to map array

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -4,14 +4,22 @@ function arrayMethod() {
   const images = document.querySelectorAll('li');
   const imageArray = Array.from(images);
 
-  imageArray.forEach((element) => {
-    console.log(element);
+  const listContainer = document.createElement('ul');
+  const target = document.querySelector('#box1');
+  target.append(listContainer);
+
+  const array2 = imageArray.map((element) => {
+    const listItem = document.createElement('li');
+    listItem.innerText = element;
+    listContainer.append(listItem);
+    return typeof element;
   });
+  console.log(array2);
 }
 
-elem.onclick = function moveCarousel() {
-  document.addEventListener('click', () => {
+function moveCarousel() {
+  document.addEventListener('click', (event) => {
     arrayMethod();
   });
-};
+}
 window.onload = moveCarousel;
