61a304d863c8574af68f9656882b8d81104f0707
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-02-12 12:34:26 -0500

    FIX js indents and function

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,15 +1,17 @@
 /* Put your javascript in here */
 
 function arrayMethod() {
-    const images = document.querySelectorAll('imageClass');
-    const imageArray = Array.from(images);
-    
-    imageArray.forEach((element) => {
-    console.log(element);
-});
+  const images = carousel.querySelectorAll('li');
+  const imageArray = Array.from(images);
+
+  let i = 1;
+  imageArray.forEach((element) => {
+    li.style.position = 'relative';
+    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+    i++;
+  });
 }
 
-function moveCarousel(){
-    document.addEventListener('click', (event) =>
-    arrayMethod();
+function moveCarousel() {
+  document.addEventListener('click', () => arrayMethod());
 }
