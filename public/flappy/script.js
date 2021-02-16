bbd87757e411bb51d9dde48d21bfa82625e4884d
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-02-16 14:55:23 -0500

    ADD obstacle style and move in js

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const gameDisplay = document.querySelector('.game-container');
   const ground = document.querySelector('.ground');
 
-  let birdLeft = 220;
+  const birdLeft = 220;
   let birdBottom = 100;
-  let gravity = 2;
+  const gravity = 2;
 
   function startGame() {
     birdBottom -= gravity;
-    bird.style.bottom = birdBottom + 'px';
-    bird.style.left = birdLeft + 'px';
+    bird.style.bottom = `${birdBottom}px`;
+    bird.style.left = `${birdLeft}px`;
   }
-  let timerId = setInterval(startGame, 20);
+  const timerId = setInterval(startGame, 20);
 
   function control(e) {
     if (e.keycode === 32) {
@@ -22,7 +22,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function jump() {
     if (birdBottom < 500) birdBottom += 50;
-    bird.style.bottom = birdBottom + 'px';
+    bird.style.bottom = `${birdBottom}px`;
   }
   document.addEventListener('keyup', control);
+
+  function generateObstacle() {
+    let obstacleLeft = 500;
+    const randomHeight = Math.random() * 60;
+    const obstacleBottom = randomHeight;
+    const obstacle = document.createElement('div');
+    obstacle.classList.add('obstacle');
+    gameDisplay.appendChild(obstacle);
+    obstacle.style.left = `${obstacleLeft}px`;
+    obstacle.style.bottom = `${obstacleBottom}px`;
+
+    function moveObstacle() {
+      obstacleLeft -= 2;
+      obstacle.style.left = `${obstacleLeft}px`;
+    }
+    const timerId = setInterval(moveObstacle, 20);
+  }
+  generateObstacle();
 });
