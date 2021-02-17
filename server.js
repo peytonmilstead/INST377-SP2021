3661d177cb0818a6088423dde04cdd46862ee563
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-02-17 17:42:02 -0500

    UPDATE res.json in server

--- server.js
+++ server.js
@@ -43,9 +43,7 @@ app.route('/api')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
-    res.set('Content-Type', 'text/html');
-    res.send('<p>Hello World</p>');
-    // res.json({data: dataToSendToFrontEnd});
+    res.json({data: 'Hello World'});
   });
 
 app.listen(port, async () => {
