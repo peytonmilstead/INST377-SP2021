9a2c3fa8816ccfd5153adaa6f7e4dd82f81b89ee
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-03-10 16:44:43 -0500

    add cp's coordinates

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,6 +1,6 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
-  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  const mymap = L.map('mapid').setView([38.99, -6], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
