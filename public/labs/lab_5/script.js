74fb52d7635f82b47b87fb16a15e79bcfcd03908
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-03-10 16:39:18 -0500

    add leaflet tutorial starting code and token

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,5 +1,14 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
+  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicGV5bWlscyIsImEiOiJja20zeXJuZ3YwN3M1MnZxcHhjMHA3OXFyIn0.5kFrQXzMQVbMwzIMvcLtzQ'
+}).addTo(mymap);
 
   return map;
 }
